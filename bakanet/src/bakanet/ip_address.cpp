f384c3fbb3f0b7adec7f70aff1abc26720b8ad2d
anulax1225 <anulax1225@icloud.com>
2024-03-02 17:30:49 +0100

    Cleaned ip_address file

--- bakanet/src/bakanet/ip_address.cpp
+++ bakanet/src/bakanet/ip_address.cpp
@@ -1,14 +1,10 @@
 #include "ip_address.h"
+#include <string.h>
 
 namespace Bk::Net {
     IpAddress::IpAddress(const char* ip)
     : str(ip)
     {
         if (inet_pton(AF_INET, str, &bytes) <= 0) perror("Bad IP");
-    }
-
-    void IpAddress::from_dns(char* dns) 
-    {
-
     }
 }
