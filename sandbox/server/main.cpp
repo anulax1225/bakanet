5bbec9ba92484e62b71e5f9bfd46b1b33656c5f9
anulax1225 <anulax1225@icloud.com>
2024-03-18 16:12:26 +0100

    Modified code to work with new module system

--- sandbox/server/main.cpp
+++ sandbox/server/main.cpp
@@ -10,7 +10,6 @@ int main()
     HttpServer server(ip, PORT);
     server.get("/", [](HttpRequest req) 
     {
-        log("Path /")
         HttpReponse res(HTTP_RES_200, req.version);
         res.body = "<p>Hello World! " + req.url + "</p>";
         return res;
@@ -18,7 +17,6 @@ int main()
 
     server.get("/home/", [](HttpRequest req) 
     {
-        log("Path /home/")
         HttpReponse res(HTTP_RES_200, req.version);
         res.body = "<p>Hello World! " + req.url + "</p>";
         return res;
