1460cc2eea00e43e2d8d7f923acc3ebfe8279e4f
anulax1225 <anulax1225@icloud.com>
2024-04-26 10:33:57 +0200

    Bug with version variable in the HTTP req but all it works

--- sandbox/server/main.cpp
+++ sandbox/server/main.cpp
@@ -1,7 +1,7 @@
 #include <bakatools.h>
 #include <bakanet.h>
 
-#define PORT 80
+#define PORT 8080
 
 using namespace Bk::Net;
 
@@ -17,11 +17,11 @@ int main()
         res.body = "<p>Coucou de /</p>";
         return res;
     });
-    server.get("/home/", [](HttpRequest& req)
+    server.post("/home/", [](HttpRequest& req)
     {
-        BK_INFO("URL /home");
-        HttpReponse res(HTTP_RES_200, req.version);
-        res.body = "<p>Coucou de /home/</p>";
+        HttpReponse res(HTTP_RES_200, "HTTP/1.1");
+        res.body = "Success";
+        BK_INFO("\n{0}\n{1}", req.body, res.to_string());
         return res;
     });
     server.start();
